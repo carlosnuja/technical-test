32c8faba53d7658055e04492551759fc79bf8560
Carlos Nuñez <carlosnuja@outlook.com>
2017-11-03 00:42:21 +0100

    Adding more files

---
+++ RestAPI/DAL/SqLite/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DAL.SqLite
+{
+    public interface IGenericRepository : IDisposable
+    {
+        T Insert<T>(T model);
+        T Update<T>(T model);
+        bool Delete<T>(T model);
+        T Select<T>(int pk) where T : new();
+        T[] SelectAll<T>() where T : new();
+    }
+}
---
+++ TestApp/TestApp/Models/ViewModels/UserViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestApp.Models.ViewModels
+{
+    public class UserViewModel
+    {
+        protected int Id { get; }
+        protected String Username { get; }
+
+    }
+}

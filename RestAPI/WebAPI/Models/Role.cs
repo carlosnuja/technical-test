11426ad0aeb93ff1bb1306a839f57f5df86a770c
Carlos Nuñez <carlosnuja@outlook.com>
2017-11-13 01:27:05 +0100

    Change db

---
+++ RestAPI/WebAPI/Models/Role.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class Role
+    {
+        [ScaffoldColumn(false)]
+        public int RoleID { get; set; }
+        [Required, StringLength(15), Display(Name = "Role")]
+        public string RoleName { get; set; }
+    }
+}

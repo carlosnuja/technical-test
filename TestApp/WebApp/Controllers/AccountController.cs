22523008b192a3d4cd82429003fe09fcc78c303b
Carlos Nuñez <carlos.nunez@sundiogroup.com>
2017-11-14 17:46:42 +0100

    More changes

---
+++ TestApp/WebApp/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Models.ViewModels;
+
+namespace WebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+		[HttpPost]
+	    public ActionResult Login(LoginViewModel model)
+	    {
+		    if (!ModelState.IsValid)
+		    {
+			    return View(model);
+		    }
+	    }
+    }
+}
--- TestApp/WebApp/Models/ViewModels/UserViewModel.cs
+++ TestApp/WebApp/Models/ViewModels/UserViewModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace TestApp.Models.ViewModels
+namespace WepApp.Models.ViewModels
 {
     public class UserViewModel
     {

d09ff05afcad3c513f14d331a06fe9d77792b06a
Carlos Nuñez <carlos.nunez@sundiogroup.com>
2017-11-14 17:25:09 +0100

    Refurbish

---
+++ TestApp/WebApp/Controllers/RestrictedAreaController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+	public class RestrictedAreaController : Controller
+	{
+		// GET: RestrictedArea/Page_1
+		public ActionResult Page_1()
+		{
+			return View();
+		}
+
+		// GET: RestrictedArea/Page_2
+		public ActionResult Page_2()
+		{
+			return View();
+		}
+
+		// GET: RestrictedArea/Page_3
+		public ActionResult Page_3()
+		{
+			return View();
+		}
+	}
+}

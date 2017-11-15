3321f4494bdbc458c50413c4c1ecabfaabf906aa
Carlos Nuñez <carlosnuja@outlook.com>
2017-11-15 01:38:01 +0100

    more changes

---
+++ TestApp/WebApp/Clients/IApiClient.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebApp.Clients
+{
+    public interface IApiClient
+    {        
+        Task<HttpResponseMessage> PostFormEncodedContent(string requestUri, params KeyValuePair<string, string>[] values);
+    }
+}
--- TestApp/WebApp/Models/ViewModels/LoginViewModel.cs
+++ TestApp/WebApp/Models/ViewModels/LoginViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,10 @@ namespace WebApp.Models.ViewModels
 {
 	public class LoginViewModel
 	{
-	}
+        [Required(ErrorMessage = "Please provide an username", AllowEmptyStrings = false)]
+        public string Username { get; set;}
+        [Required(ErrorMessage = "Please provide a password", AllowEmptyStrings = false)]
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Password)]
+        public string Password { get; set; }
+    }
 }
